9d5ae7d035fb7d72d1e864ed83c3549df2081dd5
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-09-24 21:10:25 -0600

    Se añade el form al modal

--- Tienda-Virtual/src/app/modules/product/component/category/category.component.ts
+++ Tienda-Virtual/src/app/modules/product/component/category/category.component.ts
@@ -1,25 +1,59 @@
 import { Component } from '@angular/core';
 import { CategoryService } from '../../_service/category.service';
 import { Category } from '../../_model/category';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import Swal from 'sweetalert2';
+import { SharedModule } from '../../../../shared/shared-module';
+
+
+
+declare var $: any;
 
 @Component({
   selector: 'app-category',
-  standalone: true,
-  imports: [],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
+  standalone: true,
+  imports: [SharedModule],
 })
+
+
 export class CategoryComponent {
 
   categories: Category[]= [];
-  constructor(private categoryService:CategoryService){}
 
+ 
+  form = this.formBuilder.group({
+    categoryform: ['', Validators.required],
+    tagform: ['', Validators.required],
+  });
+  
+
+  submitted = false;
+  
+  constructor(private categoryService: CategoryService, private formBuilder: FormBuilder) {
+    
+  }
+
+
+  
   ngOnInit(){
     this.categories = this.categoryService.getCategories();
   }
 
   getCategories():void{
     this.categories = this.categoryService.getCategories();
+  }
+
+  showModalForm(){
+    $("#modalForm").modal("show");
+    this.form.reset();
+  }
+
+
+
+  hideModalForm(){
+    $("#modalForm").modal("hide");
   }
 
 
