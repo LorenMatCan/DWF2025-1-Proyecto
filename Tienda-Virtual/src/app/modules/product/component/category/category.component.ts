5db6ba47ff1d3c8da2094e39319307babb044b9d
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-09-25 19:57:58 -0600

    Practica 6 completa

--- Tienda-Virtual/src/app/modules/product/component/category/category.component.ts
+++ Tienda-Virtual/src/app/modules/product/component/category/category.component.ts
@@ -4,6 +4,7 @@ import { Category } from '../../_model/category';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import Swal from 'sweetalert2';
 import { SharedModule } from '../../../../shared/shared-module';
+import { SwalMessages } from '../../../../shared/swal-messages';
 
 
 
@@ -23,13 +24,17 @@ export class CategoryComponent {
   categories: Category[]= [];
 
  
-  form = this.formBuilder.group({
-    categoryform: ['', Validators.required],
-    tagform: ['', Validators.required],
-  });
+    form = this.formBuilder.group({
+      category: ["",[Validators.required]],
+      tag: ["",[Validators.required]],
+    });
+  
+
   
 
   submitted = false;
+
+  swal: SwalMessages = new SwalMessages();
   
   constructor(private categoryService: CategoryService, private formBuilder: FormBuilder) {
     
@@ -48,8 +53,23 @@ export class CategoryComponent {
   showModalForm(){
     $("#modalForm").modal("show");
     this.form.reset();
+    this.submitted=false
   }
 
+  onSubmit():void{
+    this.submitted = true;
+    if(this.form.invalid) return;
+    this.submitted = false;
+
+    let id = this.categories.length + 1; 
+    let region = new Category(id, this.form.controls['category'].value!, this.form.controls['tag'].value!, 1);
+    this.categories.push(region);
+
+    this.hideModalForm();
+    this.swal.successMessage("La categoria ha sido registrada")
+
+
+  }
 
 
   hideModalForm(){
